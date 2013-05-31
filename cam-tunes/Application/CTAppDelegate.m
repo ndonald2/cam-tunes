adf0a724e70179fed33e1fd75d03188e1db88a1f
Nick Donaldson <ndonald2@gmail.com>
2013-05-31 10:12:06 -0400

    Reorganize project and bring in dependencies

---
+++ cam-tunes/Application/CTAppDelegate.m
@@ -0,0 +1,49 @@
+//
+//  CTAppDelegate.m
+//  cam-tunes
+//
+//  Created by Nick Donaldson on 5/31/13.
+//  Copyright (c) 2013 flatline. All rights reserved.
+//
+
+#import "CTAppDelegate.h"
+
+
+@implementation CTAppDelegate
+
+- (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
+{
+    self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
+    
+    // View controller goes here
+    
+    [self.window makeKeyAndVisible];
+    return YES;
+}
+
+- (void)applicationWillResignActive:(UIApplication *)application
+{
+
+}
+
+- (void)applicationDidEnterBackground:(UIApplication *)application
+{
+
+}
+
+- (void)applicationWillEnterForeground:(UIApplication *)application
+{
+
+}
+
+- (void)applicationDidBecomeActive:(UIApplication *)application
+{
+
+}
+
+- (void)applicationWillTerminate:(UIApplication *)application
+{
+
+}
+
+@end
